de01710f590a655f5713d77ab957680568b0774a
Ontaria Woods <ontaria.woods@gmail.com>
2023-08-23 10:30:48 -0400

    commit tree d4d8935830b6a45c137346cd75a64e7f6e16239e

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 5, 'M', 'black', 'low', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 3, 'M', 'brown', 'medium', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 4, 'F', 'black', 'low', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 8, 'M', 'brown', 'high', false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 2, 'F', 'black', 'high', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 5, 'M', 'black', 'high', false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 10, 'M', 'brown', 'medium', false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 1, 'M', 'black', 'low', true);
